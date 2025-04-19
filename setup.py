2e1ed01b8f3889ea768ce371bf6ef9dbfb957799
AmalprasadK1998 <amalprasadk1998@gmail.com>
2025-04-19 23:58:53 +0530

    Project structure ready, venv set

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Amal Prasad",
+    author_email="amalprasadk1998@gmail.com",
+    packages=find_packages()
+)
